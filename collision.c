02ddbe34dc47806a31a7e411c80524a13114b147
Tristan Penman <tristan@tristanpenman.com>
2021-03-13 17:47:41 +1100

    Allow collision test functions to return point/time of collision

--- collision.c
+++ collision.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "data.h"
 #include "entities.h"
 #include "shape.h"
@@ -18,19 +20,44 @@ static float signed_2d_triangle_area(
     return (a->x - c->x) * (b->y - c->y) - (a->y - c->y) * (b->x - c->x);
 }
 
-static bool lines_intersect(
-    const struct vec_2d *a_start, const struct vec_2d *a_end,
-    const struct vec_2d *b_start, const struct vec_2d *b_end)
+static bool lines_intersect_full(
+    const struct vec_2d *a, const struct vec_2d *b,
+    const struct vec_2d *c, const struct vec_2d *d,
+    struct vec_2d *intersection, float *t)
 {
-    const float a1 = signed_2d_triangle_area(a_start, b_end, b_end);
-    const float a2 = signed_2d_triangle_area(a_start, b_end, b_start);
-    if (a1 * a2 >= 0.0f) {
-        return false;
+    const float a1 = signed_2d_triangle_area(a, b, d);
+    const float a2 = signed_2d_triangle_area(a, b, c);
+
+    if (a1 * a2 < 0.0f) {
+        const float a3 = signed_2d_triangle_area(c, d, a);
+        const float a4 = a3 + a2 - a1;
+
+        if (a3 * a4 < 0.0f) {
+            if (intersection || t) {
+                const float toi = a3 / (a3 - a4);
+
+                if (t) {
+                    *t = toi;
+                }
+
+                if (intersection) {
+                    intersection->x = a->x + toi * (b->x - a->x);
+                    intersection->y = a->y + toi * (b->y - a->y);
+                }
+            }
+
+            return true;
+        }
     }
 
-    const float a3 = signed_2d_triangle_area(b_start, b_end, a_start);
-    const float a4 = a3 + a2 - a1;
-    return a3 * a4 < 0.0f;
+    return false;
+}
+
+static bool lines_intersect(
+    const struct vec_2d *a, const struct vec_2d *b,
+    const struct vec_2d *c, const struct vec_2d *d)
+{
+    return lines_intersect_full(a, b, c, d, NULL, NULL);
 }
 
 static struct vec_2d transform(
@@ -51,31 +78,82 @@ static struct vec_2d transform(
  *
  *****************************************************************************/
 
-bool collision_test_shape_line_segment(
+bool collision_test_shape_line_segment_full(
     const struct shape *b, const struct vec_2d *b_pos, float b_rot, float b_scale,
-    const struct vec_2d *a_start, const struct vec_2d *a_end)
+    const struct vec_2d *a_start, const struct vec_2d *a_end,
+    struct vec_2d *intersection, float *t)
 {
     const float sinr = sinf(b_rot);
     const float cosr = cosf(b_rot);
 
+    int i;
+
     if (b->num_line_segments > 0) {
-        for (int i = 0; i < b->num_line_segments * 2; i += 2) {
+        for (i = 0; i < b->num_line_segments * 2; i += 2) {
             const struct vec_2d *b1 = (struct vec_2d *) &b->vertices[2 * b->line_segments[i]];
             const struct vec_2d *b2 = (struct vec_2d *) &b->vertices[2 * b->line_segments[i + 1]];
             const struct vec_2d b_start = transform(b1, b_pos, sinr, cosr, b_scale);
             const struct vec_2d b_end = transform(b2, b_pos, sinr, cosr, b_scale);
-            if (lines_intersect(a_start, a_end, &b_start, &b_end)) {
+            if (lines_intersect_full(a_start, a_end, &b_start, &b_end, intersection, t)) {
                 return true;
             }
         }
     } else {
         const int n = b->num_vertices * 2;
-        for (int i = 0; i < n; i += 2) {
+        for (i = 0; i < n; i += 2) {
             const struct vec_2d *b1 = (struct vec_2d *) &b->vertices[i];
             const struct vec_2d *b2 = (struct vec_2d *) &b->vertices[(i + 2) % n];
             const struct vec_2d b_start = transform(b1, b_pos, sinr, cosr, b_scale);
             const struct vec_2d b_end = transform(b2, b_pos, sinr, cosr, b_scale);
-            if (lines_intersect(a_start, a_end, &b_start, &b_end)) {
+            if (lines_intersect_full(a_start, a_end, &b_start, &b_end, intersection, t)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool collision_test_shape_line_segment(
+    const struct shape *b, const struct vec_2d *b_pos, float b_rot, float b_scale,
+    const struct vec_2d *a_start, const struct vec_2d *a_end)
+{
+    return collision_test_shape_line_segment_full(
+        b, b_pos, b_rot, b_scale, a_start, a_end, NULL, NULL);
+}
+
+bool collision_test_shapes_full(
+    const struct shape *a, const struct vec_2d *a_pos, float a_rot, float a_scale,
+    const struct shape *b, const struct vec_2d *b_pos, float b_rot, float b_scale,
+    struct vec_2d *intersection, float *t)
+{
+    // TODO: perform low granularity test using bounding sphere
+
+    const float sinr = sinf(a_rot);
+    const float cosr = cosf(a_rot);
+
+    int i;
+
+    if (a->num_line_segments > 0) {
+        for (i = 0; i < a->num_line_segments * 2; i += 2) {
+            const struct vec_2d *a1 = (struct vec_2d *) &a->vertices[2 * a->line_segments[i]];
+            const struct vec_2d *a2 = (struct vec_2d *) &a->vertices[2 * a->line_segments[i + 1]];
+            const struct vec_2d a_start = transform(a1, a_pos, sinr, cosr, a_scale);
+            const struct vec_2d a_end = transform(a2, a_pos, sinr, cosr, a_scale);
+            if (collision_test_shape_line_segment_full(
+                    b, b_pos, b_rot, b_scale, &a_start, &a_end, intersection, t)) {
+                return true;
+            }
+        }
+    } else {
+        const int n = a->num_vertices * 2;
+        for (i = 0; i < n; i += 2) {
+            const struct vec_2d *a1 = (struct vec_2d *) &a->vertices[i];
+            const struct vec_2d *a2 = (struct vec_2d *) &a->vertices[(i + 2) % n];
+            const struct vec_2d a_start = transform(a1, a_pos, sinr, cosr, a_scale);
+            const struct vec_2d a_end = transform(a2, a_pos, sinr, cosr, a_scale);
+            if (collision_test_shape_line_segment_full(
+                    b, b_pos, b_rot, b_scale, &a_start, &a_end, intersection, t)) {
                 return true;
             }
         }
@@ -88,33 +166,6 @@ bool collision_test_shapes(
     const struct shape *a, const struct vec_2d *a_pos, float a_rot, float a_scale,
     const struct shape *b, const struct vec_2d *b_pos, float b_rot, float b_scale)
 {
-    // TODO: perform low granularity test using bounding sphere
-
-    const float sinr = sinf(a_rot);
-    const float cosr = cosf(a_rot);
-
-    if (a->num_line_segments > 0) {
-        for (int i = 0; i < a->num_line_segments * 2; i += 2) {
-            const struct vec_2d *a1 = (struct vec_2d *) &a->vertices[2 * a->line_segments[i]];
-            const struct vec_2d *a2 = (struct vec_2d *) &a->vertices[2 * a->line_segments[i + 1]];
-            const struct vec_2d a_start = transform(a1, a_pos, sinr, cosr, a_scale);
-            const struct vec_2d a_end = transform(a2, a_pos, sinr, cosr, a_scale);
-            if (collision_test_shape_line_segment(b, b_pos, b_rot, b_scale, &a_start, &a_end)) {
-                return true;
-            }
-        }
-    } else {
-        const int n = a->num_vertices * 2;
-        for (int i = 0; i < n; i += 2) {
-            const struct vec_2d *a1 = (struct vec_2d *) &a->vertices[i];
-            const struct vec_2d *a2 = (struct vec_2d *) &a->vertices[(i + 2) % n];
-            const struct vec_2d a_start = transform(a1, a_pos, sinr, cosr, a_scale);
-            const struct vec_2d a_end = transform(a2, a_pos, sinr, cosr, a_scale);
-            if (collision_test_shape_line_segment(b, b_pos, b_rot, b_scale, &a_start, &a_end)) {
-                return true;
-            }
-        }
-    }
-
-    return false;
+    return collision_test_shapes_full(
+        a, a_pos, a_rot, a_scale, b, b_pos, b_rot, b_scale, NULL, NULL);
 }
--- collision.h
+++ collision.h
@@ -6,9 +6,19 @@
 struct shape;
 struct vec_2d;
 
+bool collision_test_shape_line_segment_full(
+    const struct shape *a, const struct vec_2d *a_pos, float a_rot, float a_scale,
+    const struct vec_2d *line_start, const struct vec_2d *line_end,
+    struct vec_2d *intersection, float *t);
+
 bool collision_test_shape_line_segment(
     const struct shape *a, const struct vec_2d *a_pos, float a_rot, float a_scale,
     const struct vec_2d *line_start, const struct vec_2d *line_end);
+
+bool collision_test_shapes_full(
+    const struct shape *a, const struct vec_2d *a_pos, float a_rot, float a_scale,
+    const struct shape *b, const struct vec_2d *b_pos, float b_rot, float b_scale,
+    struct vec_2d *intersection, float *t);
 
 bool collision_test_shapes(
     const struct shape *a, const struct vec_2d *a_pos, float a_rot, float a_scale,
